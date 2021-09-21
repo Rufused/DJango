d2c12fbe8380c5c311909abf7461608fac55ff1a
Rufused <perpetuum.coding@gmail.com>
2021-09-21 18:30:54 +0300

    m

--- Djangos/urls.py
+++ Djangos/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from users.views import users, create
+from django.urls import path, include
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('create/<str:name>/<int:age>', create),
-    path('users', users)
+    path('cars', include('car.urls'))
 ]
